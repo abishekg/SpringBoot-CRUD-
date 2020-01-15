cffc8ec80238d102bda3d19de98d920ba863e7e9
Abishek G <abishek.g@mrcooper.com>
2020-01-15 15:29:34 +0530

    Basic CRUD using SpringBoot without DB

---
+++ src/main/java/com/example/freecodecamp/FreeCodeCampApplication.java
@@ -0,0 +1,12 @@
+package com.example.freecodecamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeCodeCampApplication {
+
+    public static void main(String[] args) {
+    	SpringApplication.run(FreeCodeCampApplication.class, args);
+    }
+}
